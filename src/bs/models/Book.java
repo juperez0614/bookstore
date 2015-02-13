6c7afe87168386e6862e938195aff47e79f2c1c2
asmusj224 <asmusj@uw.edu>
2015-02-12 16:34:18 -0800

    project clean up

--- src/bs/models/Book.java
+++ src/bs/models/Book.java
@@ -5,5 +5,37 @@ import java.io.Serializable;
 public class Book implements Serializable {
 	private static final long serialVersionUID = 1L;
 	private int ISBN; 
+	private double price; 
+	private String summary; 
+	
+	public Book(){}
+	
+	public Book(int ISBN, double price, String summary){
+		this.ISBN = ISBN;
+		this.price = price; this.summary = summary; 
+	}
 
+	public int getISBN() {
+		return ISBN;
+	}
+
+	public void setISBN(int iSBN) {
+		ISBN = iSBN;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	public String getSummary() {
+		return summary;
+	}
+
+	public void setSummary(String summary) {
+		this.summary = summary;
+	}
 }
